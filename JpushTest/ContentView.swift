977168dcbfccaba0ef6a7fb466ed9a83b8bf77ea
aidenhsy <aidenhsy@hotmail.com>
2023-12-02 05:20:52 +0800

    Initial Commit

---
+++ JpushTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JpushTest
+//
+//  Created by Aiden Yang on 2023/12/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JpushTest/JpushTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JpushTestApp.swift
+//  JpushTest
+//
+//  Created by Aiden Yang on 2023/12/2.
+//
+
+import SwiftUI
+
+@main
+struct JpushTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
